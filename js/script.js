81a6a97c703452680d055d6ec2b57405f6a670ab
falkAnonymous <davidmartinsantos77@gmail.com>
2023-05-23 15:16:53 -0300

    atualizado

--- js/script.js
+++ js/script.js
@@ -7,33 +7,33 @@ function circulo() {
 }
 
 function hipotenusa() {
-    let co;
-    let ca;
-    let h = Math.pow(co = prompt(), 2) + Math.pow(ca = prompt(), 2);
-    alert(Math.pow(h, 2));
+    let co = parseInt(document.getElementById('hipotenusa1').value);
+    let ca = parseInt(document.getElementById('hipotenusa2').value);
+    let h = Math.sqrt(Math.pow(co, 2) + Math.pow(ca, 2)).toFixed(2);
+    document.getElementById('hipotenusa-resultado').innerHTML = h;
 }
 function realDolar() {
     let TaxaCambio = 4.951;
-    let real = prompt();
-    let result = real / TaxaCambio;
-    let dolar = result;
-    alert(dolar)
+    let real = document.getElementById('Real').value;
+    let dolar = (real / TaxaCambio).toFixed(2);
+    document.getElementById('Resultado-real').innerHTML = dolar
+
 }
 function Fahrenheit() {
-    let Celsius = prompt()
-    let Fahrenheit = Celsius * 9 / 5 + 32
+    let Celsius = parseInt(document.getElementById('Celsus').value)
+    let Fahrenheit = Celsius * 9 / 5 + 32;
     let resultFahrenheit = Fahrenheit
-    alert(resultFahrenheit)
-
+    document.getElementById('resultado-celsus').innerHTML = resultFahrenheit + "Fº";
 }
 function Baskhara1() {
     let a, b, c;
-    a = parseInt(prompt());
-    b = parseInt(prompt());
-    c = parseInt(prompt());
+    a = parseInt(document.getElementById('A').value);
+    b = parseInt(document.getElementById('B').value);
+    c = parseInt(document.getElementById('C').value);
     let delta = Math.pow(b, 2) - 4 * a * c;
     let x1 = (-b + Math.sqrt(delta)) / (2 * a)
     let x2 = (-b - Math.sqrt(delta)) / (2 * a)
+    document.getElementById("resultado-baskx1")
     alert(x1 + "\n" + x2)
 
 }
