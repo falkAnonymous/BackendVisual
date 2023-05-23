4c57429fb9ed6e1e7e73f513d31b42129c2bc573
falkAnonymous <davidmartinsantos77@gmail.com>
2023-05-23 20:29:07 -0300

    Atualizando o site

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ function circulo() {
     let raio = parseInt(document.getElementById('circulo1').value);
     const result = PI.toFixed(2) * Math.pow(raio, 2).toFixed(2);
     document.getElementById('circulo-resultado').innerHTML = result;
-    
+
 }
 
 function hipotenusa() {
@@ -13,10 +13,10 @@ function hipotenusa() {
     document.getElementById('hipotenusa-resultado').innerHTML = h;
 }
 function realDolar() {
-    let TaxaCambio = 4.951;
-    let real = document.getElementById('Real').value;
-    let dolar = (real * TaxaCambio).toFixed(2);
-    document.getElementById('Resultado-real').innerHTML = "$"+dolar
+    let TaxaCambio = 4.951.toFixed(2);
+    let dolar = document.getElementById('Real').value;
+    let real = (dolar * TaxaCambio).toFixed(2);
+    document.getElementById('Resultado-real').innerHTML = "R$" + real
 
 }
 function Fahrenheit() {
@@ -28,7 +28,7 @@ function Fahrenheit() {
     //     document.getElementById('resultado-celsus').innerHTML = "Não é possível calcular por zero";
     // }
 }
-function Baskhara1(){
+function Baskhara1() {
     let a, b, c;
     a = parseInt(document.getElementById('A').value);
     b = parseInt(document.getElementById('B').value);
@@ -36,16 +36,27 @@ function Baskhara1(){
     let delta = Math.pow(b, 2) - 4 * a * c;
     let x1 = (-b + Math.sqrt(delta)) / (2 * a)
     let x2 = (-b - Math.sqrt(delta)) / (2 * a)
-    document.getElementById("resultado-baskx1").innerHTML = "X1"+ x1.toFixed(2)
-    document.getElementById("resultado-baskx2").innerHTML = "X2" + x2.toFixed(2)
+    document.getElementById("resultado-baskx1").innerHTML = "X1 = " + x1.toFixed(2)
+    document.getElementById("resultado-baskx2").innerHTML = "X2 = " + x2.toFixed(2)
     // if(a == 0 && b == 0 && c == 0){
     //     document.getElementById("resultado-baskx1").innerHTML = "Não é possível calcular por zero"
     //     document.getElementById("resultado-baskx2").innerHTML = "Não é possível calcular por zero"
     // }
 }
-function Retangulo(){
+function Retangulo() {
     base = parseInt(document.getElementById('retangulo1').value);
     altura = parseInt(document.getElementById('retangulo2').value);
     retangulo = base * altura;
     document.getElementById('retangulo-resultado').innerHTML = retangulo;
+}
+function Triangulo() {
+    base = parseInt(document.getElementById('triangulo1').value)
+    altura = parseInt(document.getElementById('triangulo2').value)
+    area = ((base * altura) / 2).toFixed(2)
+    document.getElementById('resultado-triangulo').innerHTML = area;
+}
+km_ms = () => {
+    KmHora = document.getElementById('Km').value
+    mSegundo = (KmHora / 3.6).toFixed(2)
+    document.getElementById('resultado-km').innerHTML = mSegundo + " ms"
 }
