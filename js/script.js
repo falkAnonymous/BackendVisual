2da1322264b110adf32181c10fd6ac99420c3472
falkAnonymous <davidmartinsantos77@gmail.com>
2023-05-24 16:59:23 -0300

    Atualizando o codigo js

--- js/script.js
+++ js/script.js
@@ -34,8 +34,20 @@ function Baskhara1() {
     b = parseInt(document.getElementById('B').value);
     c = parseInt(document.getElementById('C').value);
     let delta = Math.pow(b, 2) - 4 * a * c;
+
+    if (delta < 0 || (a === 0 && b === 0)) {
+        document.getElementById('resultado-baskx1').innerHTML = ("Erro: A equação não possui solução real.");
+        document.getElementById('resultado-baskx2').innerHTML = ("Erro: A equação não possui solução real.");
+        return;
+    }
     let x1 = (-b + Math.sqrt(delta)) / (2 * a)
     let x2 = (-b - Math.sqrt(delta)) / (2 * a)
+
+    if (isNaN(x1) || isNaN(x2)) {
+        document.getElementById('resultado-baskx1').innerHTML = ("Erro: A equação possui solução imaginária.");
+        document.getElementById('resultado-baskx2').innerHTML = ("Erro: A equação possui solução imaginária.");
+        return;
+    }
     document.getElementById("resultado-baskx1").innerHTML = "X1 = " + x1.toFixed(2)
     document.getElementById("resultado-baskx2").innerHTML = "X2 = " + x2.toFixed(2)
     // if(a == 0 && b == 0 && c == 0){
@@ -56,9 +68,16 @@ function Triangulo() {
     document.getElementById('resultado-triangulo').innerHTML = area;
 }
 km_ms = () => { //Função anônima
-    KmHora = document.getElementById('Km').value
+    KmHora = parseInt(document.getElementById('Km').value);
     mSegundo = (KmHora / 3.6).toFixed(2)
-    document.getElementById('resultado-km').innerHTML = mSegundo + " ms"
+    if(mSegundo <= 0 ){
+        document.getElementById('resultado-km').innerHTML = "Não é possível Converter 0kms em Ms"
+    }else if(mSegundo == isNaN){
+        document.getElementById('resultado-km').innerHTML = " Ponha um numero valido" 
+    }else{
+        document.getElementById('resultado-km').innerHTML = mSegundo + " ms"
+    }
+    
 }
 function Media() {
     nota1 = parseFloat(document.getElementById('Nota1').value);
@@ -67,4 +86,15 @@ function Media() {
     media = ((nota1 + nota2 + nota3) / 3).toFixed(2)
     document.getElementById('resultado-media').innerHTML = "Media Final: " + media
 
+}
+function Dividir() {
+    numerador = parseFloat(document.getElementById().value);
+    denominador = parseFloat(document.getElementById().value);
+    quociente = (numerador / denominador).toFixed(2);
+    if(quociente == 0){
+        document.getElementById().innerHTML = "Não existe divisão divisão por 0";
+    }
+    else{
+        document.getElementById().innerHTML = quociente; 
+    }
 }
